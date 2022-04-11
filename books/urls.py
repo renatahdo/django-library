446a929812fc929a47f22fa55e8fd86070498a52
Renata Hurtado <renatah2198@gmail.com>
2022-04-11 16:02:09 +0000

    First commit

---
+++ books/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from rest_framework import routers
+from django.urls import path, include
+
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+router.register(r'author', views.AuthorViewSet)
+router.register(r'genre', views.GenreViewSet)
+router.register(r'language', views.LanguageViewSet)
+router.register(r'booksauthor', views.BooksAuthorsViewSet)
+router.register(r'booksgenre', views.BooksGenresViewSet)
+router.register(r'bookslanguage', views.BooksLanguagesViewSet)
+
+urlpatterns = [
+	path('', include(router.urls)),
+]
---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+# Serializadores son los que se encargan de convertir en binario
+# todos los datos que se envían desde o hacia nuestro modelo.
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
